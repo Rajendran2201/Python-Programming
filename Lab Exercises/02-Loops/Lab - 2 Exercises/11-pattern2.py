be61b6935480249d899894e010489f504694357c
RAJENDRAN S <137254223+Rajendran2201@users.noreply.github.com>
2024-02-22 15:42:14 +0530

    Update 11-pattern2.py

--- Lab Exercises/02-Loops/Lab - 2 Exercises/11-pattern2.py
+++ Lab Exercises/02-Loops/Lab - 2 Exercises/11-pattern2.py
@@ -3,12 +3,13 @@ Write a python program to print a diamond pattern
 
 '''
 
-n = int(input("Enter the number of rows : "))
+count = int(input("Enter the number of rows : "))
+n = int(count/2)
 
 for i in range(n):
     print(" "*(n-i+1), "*"*(i*2+1))
 
-for i in range(n-2, -1, -1):
+for i in range(n, -1, -1):
     print(" "*(n-i+1), "*"*(i*2+1))
 
 
@@ -18,25 +19,17 @@ OUTPUT - 1 :
 
 Enter the number of rows : 3
 
-     *
-    ***
-   *****
-    ***
-     *
-
+   *
+  ***
+   *
 
 OUTPUT - 2  :
 
 Enter the number of rows : 5
 
-       *    
-      ***   
-     *****  
-    ******* 
-   *********
-    ******* 
-     *****  
-      ***   
-       *    
-
+    *
+   ***
+  *****
+   ***
+    *
 '''
