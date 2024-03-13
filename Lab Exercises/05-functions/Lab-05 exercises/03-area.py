24985b30e1152eca4e5605604e20525768cba102
RAJENDRAN S <137254223+Rajendran2201@users.noreply.github.com>
2024-03-13 11:56:46 +0530

    Update 03-area.py

--- Lab Exercises/05-functions/Lab-05 exercises/03-area.py
+++ Lab Exercises/05-functions/Lab-05 exercises/03-area.py
@@ -8,7 +8,7 @@ import math
 
 def caculate_area(radius)->float:
     '''This function takes radius as input and returns the area of the circle'''
-    area = math.pi * (radius**radius)
+    area = math.pi * (radius**2)
     return area
 
 radius = float(input("Enter the radius : "))
@@ -21,6 +21,6 @@ print("The area of the circle of radius {} is {}".format(radius, area))
 OUTPUT :
 
 Enter the radius : 3
-The area of the circle of radius 3.0 is 84.82300164692441
+The area of the circle of radius 3.0 is 28.274333882308138
 
 '''
