2393d5655e8e64a06077c292ba40ad43ec1ed09c
RAJENDRAN S <137254223+Rajendran2201@users.noreply.github.com>
2024-03-20 09:05:00 +0530

    Create string_concatenation.py

---
+++ Strings/string_concatenation.py
@@ -0,0 +1,13 @@
+'''Given a list of words, concatenate them into a single string seperated by spaces'''
+
+my_words = ['This', 'is', 'a', 'sample', 'text']
+print(" ".join(my_words))
+
+
+'''
+
+OUTPUT : 
+
+This is a sample text
+
+'''
