1e89ca436713c5e09b98742b07e0d5b364d2ccf4
Raj <137254223+Rajendran2201@users.noreply.github.com>
2024-05-23 23:27:02 +0530

    Create strings.py

---
+++ Practice Problems/strings.py
@@ -0,0 +1,10 @@
+'''
+ Create a string made of the first, middle and last character
+
+'''
+
+string = "Rajendran"
+print(string[0], end="")
+length = len(string)
+print(string[length//2], end="")
+print(string[-1])
